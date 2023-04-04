cfbd2ad09e297c92d6ca33352404607fffc8dbcf
Harim <radoharim@gmail.com>
2023-04-04 21:53:04 +0200

    Création d'un endpoint pour supprimer une carte

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 
 
 use App\Entity\Carte;
+use App\Repository\CarteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -47,5 +49,20 @@ class CarteController extends AbstractController
             $data['number']=$card->getNumber();
         }
         return $this->json(['cards' => $result]);
+    }
+
+    /**
+     * Delete Carte
+     * @Route("/api/card/{id_card}", name="delete_carte", methods={"DELETE"})
+     */
+    public function deleteCarte($id_card, Request $request, EntityManagerInterface $entityManager, CarteRepository $carteRepository): Response
+    {
+        $card = $carteRepository->find($id_card);
+        if(!$card instanceof Carte){
+            return new JsonResponse(['code'=>Response::HTTP_NOT_FOUND, 'message'=>'Card not found'], Response::HTTP_NOT_FOUND, []);
+        }
+        $entityManager->remove($card);
+        $entityManager->flush();
+        return $this->json(['message' => "Success"]);
     }
 }
